17e1e061c89fc8098752e9b513f71b4cb1f2144c
Heinrich Klobuczek <heinrich@mail.com>
2022-01-04 13:35:20 -0900

    cleanup

--- app/mailers/bug_mailer.rb
+++ app/mailers/bug_mailer.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class BugMailer < ApplicationMailer
   def bug
     mail(to: 'someone@somewhere.com')
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,5 @@
 require_relative "boot"
 
-require "active_model/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
