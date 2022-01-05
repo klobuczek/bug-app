0d6b604d6d35a692c3d2425b5b60e8c2e42c383e
Heinrich Klobuczek <heinrich@mail.com>
2022-01-04 17:05:20 -0900

    further reduction

--- app/mailers/bug_mailer.rb
+++ app/mailers/bug_mailer.rb
@@ -1,5 +1,5 @@
-class BugMailer < ApplicationMailer
+class BugMailer < ActionMailer::Base
   def bug
-    mail(to: 'someone@somewhere.com')
+    mail(to: 'someone@somewhere.com', from: 'from@example.com', body: '')
   end
 end
